6df5ae79522fd05b5b6b80399beb92b197960971
Dinodup <dudup2626@gmail.com>
2024-06-18 16:19:47 -0300

    comita

---
+++ exercicios/ex6.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>A medida para a área será: </h1>
+    <?php
+    $t1 = $_POST["texto1"];
+    $t2 = $_POST["texto2"];
+
+    echo "Comprimento: ".$t1;
+    echo "Largura: ".$t2;
+    $a = $t1 * $t2;
+    echo "Área: ".$a;
+    ?>
+</body>
+</html>
