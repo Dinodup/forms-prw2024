e6f3099ae6a860f4535aae74f72440c5a7f9a31c
Dinodup <dudup2626@gmail.com>
2024-06-18 10:46:18 -0300

    comita

---
+++ form2.php
@@ -0,0 +1,13 @@
+<?php
+    echo "<h2>Cadastro</h2>";
+
+    // $_GET['nome_campo_form_html']
+    // $_POST['nome_campo_form_html']
+
+    //Recebendo dado enviado
+    //Do form html via post
+
+    $nome = $_POST['nome'];
+     echo "Nome: ";
+     echo $nome;
+?>
