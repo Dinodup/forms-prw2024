2468e4e7d2f8b137da5a98836098785e97c1fbc7
Dinodup <dudup2626@gmail.com>
2024-06-18 17:00:36 -0300

    comita

---
+++ exercicios/ex9.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Resultados</h1>
+    <?php
+    $valors = $_POST["nums"];
+
+    for($i=0; )
+    
+    $ddd = $idades * 365;
+    echo "A pessoa completou com essa idade: ".$ddd." dias de vida<br>";
+    ?>
+</body>
+</html>
